e0ef6287d0296923b42672697f71b3dcca6e1f26
raguljeeva20 <123439943+raguljeeva20@users.noreply.github.com>
2023-03-10 01:12:48 +0530

    Add files via upload

---
+++ signing.js
@@ -0,0 +1,88 @@
+
+let namespan=document.getElementById("namespan");
+let mailspan=document.getElementById("mailspan");
+let dobspan=document.getElementById("dobspan");
+let phspan=document.getElementById("phspan");
+let submitspan=document.getElementById("submitspan");
+
+function validatename()
+{
+    let name=document.getElementById("name").value;
+    
+    
+    if (!name.match(/^[A-za-z]*$/))
+     {
+        namespan.innerHTML="Give Valid Name";
+        return false;
+     } 
+    namespan.innerHTML="Valid✅";
+    namespan.style.color="white";
+    namespan.style.marginLeft="80%";
+
+    return true;
+}
+
+function validatephone()
+{
+    let phone=document.getElementById("phno").value;
+    let exp= /^[0-9]*$/;
+    if (!phone.match(exp))
+    {
+        phspan.innerHTML="Invalid phno";
+        return false;
+    }
+    if (phone.length!=10)
+    {
+        phspan.innerHTML="ph-no should be 10 digits";
+        return false;
+    }
+    phspan.innerHTML="Valid✅";
+    phspan.style.color="white";
+    phspan.style.marginLeft="80%";
+    return true;
+
+}
+function validatemail()
+{
+    let email=document.getElementById("mail").value;
+    if(email.length==0)
+    {
+        mailspan.innerHTML="Email is Required";
+        return false;
+    }
+    if (!email.match(/^[A-Z a-z0-9]*[@][a-zA-Z]*[\.][a-z]{2,4}$/)) {
+        mailspan.innerHTML="Email Invalid";
+        return false;
+    }
+    mailspan.innerHTML="Valid✅";
+    mailspan.style.color="white";
+    mailspan.style.marginLeft="80%";
+    return true;
+}
+function validatedob()
+{
+    let dat=document.getElementById("dob").value;
+    if (dat=='')
+    {
+        dobspan.innerHTML="DOB Required";
+        return false;
+    }
+    
+    dobspan.innerHTML="Valid✅";
+    dobspan.style.color="white";
+    dobspan.style.marginLeft="80%";
+    return true;
+    
+}
+function validatesub()
+{
+  if (validatedob()&&validatemail()&&validatephone()&&validatename())
+  
+    {
+    location.reload();
+    }
+    else{
+        submitspan.innerHTML="please fill all details correctly"
+    }
+    
+}
